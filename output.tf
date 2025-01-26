694f0c81601e4414eb0ce4fcaf9f7bce57ccc37a
aian <53809101+deeowemez@users.noreply.github.com>
2025-01-27 00:06:05 +0800

    add ig, route table, subnet creation and association

--- output.tf
+++ output.tf
@@ -1,41 +1,65 @@
 # VPC Output Values
 output "vpc_id" {
-  description = "The ID of the created VPC"
-  value       = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "The IDs of the created subnets"
-  value       = module.vpc.public_subnet_ids
+  value = module.vpc.public_subnet_ids
 }
 
 output "private_app_subnet_ids" {
-  description = "The IDs of the created subnets"
-  value       = module.vpc.private_app_subnet_ids
+  value = module.vpc.private_app_subnet_ids
 }
 
 output "private_data_subnet_ids" {
-  description = "The IDs of the created subnets"
-  value       = module.vpc.private_data_subnet_ids
+  value = module.vpc.private_data_subnet_ids
+}
+
+output "internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
+
+output "aws_eip_id" {
+  value = module.vpc.aws_eip_id
+}
+
+output "aws_nat_gw_id" {
+  value = module.vpc.aws_nat_gw_id
+}
+
+output "public_subnet_route_table_associations" {
+  value = module.vpc.public_subnet_route_table_associations
+}
+
+output "private_appserver_subnet_route_table_associations" {
+  value = module.vpc.private_appserver_subnet_route_table_associations
+}
+
+output "private_dataserver_subnet_route_table_associations" {
+  value = module.vpc.private_dataserver_subnet_route_table_associations
+}
+
+output "aws_vpc_endpoint_dynamo_id" {
+  value = module.vpc.aws_vpc_endpoint_dynamo_id
+}
+
+output "aws_vpc_endpoint_dynamo_route_table_associations" {
+  value = module.vpc.aws_vpc_endpoint_dynamo_route_table_associations
 }
 
 # S3 Bucket Output Values
 output "s3_bucket_name" {
-  description = "The name of the bucket"
-  value       = module.s3.s3_bucket_name
+  value = module.s3.s3_bucket_name
 }
 
 output "s3_bucket_domain_name" {
-  description = "The domain name of the bucket"
-  value       = module.s3.s3_bucket_domain_name
+  value = module.s3.s3_bucket_domain_name
 }
 
 output "website_domain" {
-  description = "The domain name of the website endpoint"
-  value       = module.s3.website_domain
+  value = module.s3.website_domain
 }
 
 output "website_endpoint" {
-  description = "The website endpoint"
-  value       = module.s3.website_endpoint
+  value = module.s3.website_endpoint
 }
--- vpc/output.tf
+++ vpc/output.tf
@@ -17,3 +17,48 @@ output "private_data_subnet_ids" {
   description = "The IDs of the private data subnets"
   value       = aws_subnet.private_data[*].id
 }
+
+output "internet_gateway_id" {
+  description = "The ID of the created internet gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "aws_eip_id" {
+  description = "The allocated elastic ip address"
+  value       = aws_eip.nat_gw[*].id
+}
+
+output "aws_nat_gw_id" {
+  description = "The ID of the created nat gateway"
+  value       = aws_nat_gateway.nat_gw[*].id
+}
+
+output "public_subnet_route_table_associations" {
+  description = "Mapping of public subnets to their associated route tables"
+  value = { for idx, subnet_id in aws_subnet.public[*].id :
+    subnet_id => aws_route_table.public.id
+  }
+}
+
+output "private_appserver_subnet_route_table_associations" {
+  description = "Mapping of appserver subnets to their associated route tables"
+  value = { for idx, subnet_id in aws_subnet.private_app[*].id :
+    subnet_id => aws_route_table.appserver[idx].id }
+}
+
+output "private_dataserver_subnet_route_table_associations" {
+  description = "Mapping of dataserver subnets to their associated route tables"
+  value = { for idx, subnet_id in aws_subnet.private_data[*].id :
+    subnet_id => aws_vpc.main.default_route_table_id }
+}
+
+output "aws_vpc_endpoint_dynamo_id" {
+  description = "The ID of the DynamoDB VPC Gateway Endpoint"
+  value       = aws_vpc_endpoint.dynamo.id
+}
+
+output "aws_vpc_endpoint_dynamo_route_table_associations" {
+  description = "Mapping of vpc endpoints to their associated route tables"
+  value = { for idx, route_table_id in aws_route_table.appserver[*].id :
+    route_table_id => aws_vpc_endpoint.dynamo.id }
+}
